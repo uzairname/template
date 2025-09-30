b289d1e27e39b564b186122d13a354d7ad7d382a
Uzair <uzair.m6d@gmail.com>
2025-09-30 18:51:43 -0400

    update action

--- terraform/modules/cloudflare/main.tf
+++ terraform/modules/cloudflare/main.tf
@@ -55,19 +55,4 @@ variable "supabase_project_id" {
 resource "cloudflare_worker" "worker" {
   account_id = var.account_id
   name = var.worker_name
-
-  provisioner "local-exec" {
-    command = "./scripts/upload-secrets.sh"
-    environment = {
-      CLOUDFLARE_ACCOUNT_ID        = var.account_id
-      CLOUDFLARE_API_TOKEN         = var.cloudflare_api_token
-      CLOUDFLARE_WORKER_NAME       = var.worker_name
-
-      SUPABASE_URL                 = var.supabase_url
-      SUPABASE_ANON_KEY            = var.supabase_anon_key
-      SUPABASE_SERVICE_ROLE_KEY    = var.supabase_service_role_key
-      SUPABASE_DB_PASSWORD         = var.supabase_db_password
-      SUPABASE_PROJECT_ID          = var.supabase_project_id
-    }
-  }
 }
