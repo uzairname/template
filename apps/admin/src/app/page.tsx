4e83318976b343197387d2e938ac7fbe51d807f1
Uzair <uzair.m6d@gmail.com>
2025-10-02 21:40:45 -0400

    use sentry

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -28,7 +28,7 @@ import { LogIn, LogOut, User, UserCheck, UserPlus } from 'lucide-react'
 function HomeContent() {
   const { isMobile } = useSidebar()
   const { user, signOut } = useAuth()
-  throw new Error("notimplemented")
+  if (!!LogIn) throw new Error("notimplemented")
   return (
     <>
       <Sidebar collapsible="offcanvas" variant="inset">
