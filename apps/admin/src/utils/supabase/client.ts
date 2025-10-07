84645b9d4aebca6a95824a9e03596583722ab17e
Uzair <uzair.m6d@gmail.com>
2025-10-07 01:33:48 -0400

    update supabase client

--- apps/admin/src/utils/supabase/client.ts
+++ apps/admin/src/utils/supabase/client.ts
@@ -6,16 +6,16 @@ export function createClient() {
   return createBrowserClient(
     nonNullable(process.env.NEXT_PUBLIC_SUPABASE_URL, 'NEXT_PUBLIC_SUPABASE_URL'),
     nonNullable(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY, 'NEXT_PUBLIC_SUPABASE_ANON_KEY'),
-    {
-      cookieOptions: {
-        domain:
-          process.env.NEXT_PUBLIC_ENVIRONMENT === 'production'
-            ? '.uzairname.org' // Shared domain for Cloudflare Workers subdomains
-            : 'localhost', // For local development
-        path: '/',
-        sameSite: process.env.NEXT_PUBLIC_ENVIRONMENT === 'production' ? 'none' : 'lax', // 'none' for cross-site cookies in production (with HTTPS), 'lax' for local dev
-        secure: process.env.NEXT_PUBLIC_ENVIRONMENT === 'production', // Only send cookies over HTTPS in production
-      },
-    }
+    // {
+    //   cookieOptions: {
+    //     domain:
+    //       process.env.NEXT_PUBLIC_ENVIRONMENT === 'production'
+    //         ? '.uzairname.org'
+    //         : 'localhost',
+    //     path: '/',
+    //     sameSite: process.env.NEXT_PUBLIC_ENVIRONMENT === 'production' ? 'none' : 'lax', // 'none' for cross-site cookies in production (with HTTPS), 'lax' for local dev
+    //     secure: process.env.NEXT_PUBLIC_ENVIRONMENT === 'production', // Only send cookies over HTTPS in production
+    //   },
+    // }
   )
 }
