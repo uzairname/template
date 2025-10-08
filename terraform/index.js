2de5168324f42e5a647aa22962b4937c4d7d0938
Uzair <uzair.m6d@gmail.com>
2025-10-07 22:57:58 -0400

    use cloudflare_workers_deployment and cloudflare_workers_version in terraform

--- terraform/index.js
+++ terraform/index.js
@@ -1,7 +1,10 @@
 
-// worker.js
+// This is the temporary script for the downtime between provisioning resources and deployment
+// The "cloudflare_worker_version" resource requires a script to be uploaded.
+// This script is replaced with the production one by opennextjs-cloudflare in .github/workflows/deploy.yml
+
 export default {
   async fetch(request, env, ctx) {
-    return new Response(`Hello`);
-  },
+    return new Response(`This site is under maintenance. Please check back in a few moments.`);
+  }
 };
